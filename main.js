b1fedbec7f1568aaf63376dd7e242bc8aeb83c39
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 21:56:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let num = 2024;
+console.log(`My Favorite Number Is ${num}`);
